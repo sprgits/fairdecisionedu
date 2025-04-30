d11395b39dcb697eb3d5d94cef3ad5eff0551d3f
sbrpgr <sprbxr@gmail.com>
2025-04-30 16:13:11 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Dashboard from './pages/Dashboard';
 import CaseAnalysis from './pages/CaseAnalysis';
